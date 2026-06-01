175ddce85c5a1e61b8554696876bb8e77f42a873
Muhammad Enuh <132081071+usernamenuh@users.noreply.github.com>
2026-06-01 15:37:09 +0700

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://loving-adventure-production-6fbd.up.railway.app/api/v1";
+  import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
